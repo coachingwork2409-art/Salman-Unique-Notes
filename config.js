f96365559959c28763425dd0c12baa280ada2176
salman nafis <coachingwork2409@gmail.com>
2025-11-29 10:52:20 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Configuration - edit AFTER you deploy the backend
-window.SUN_CONFIG = {
-  BACKEND: 'https://YOUR_BACKEND_DOMAIN', // e.g. https://nafis-server.up.railway.app
-  PRICE_INR: 50,
-  PRODUCT_ID: 'mybook'
-};
