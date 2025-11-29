9241c3b8f1982d9de4ebf65f40780cbe7779111f
salman nafis <coachingwork2409@gmail.com>
2025-11-29 11:16:38 +0530

    assets/js/config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+// Configuration - edit AFTER you deploy the backend
+window.SUN_CONFIG = {
+  BACKEND: 'https://YOUR_BACKEND_DOMAIN', // e.g. https://nafis-server.up.railway.app
+  PRICE_INR: 50,
+  PRODUCT_ID: 'mybook'
+};
