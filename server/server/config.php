daeef17eaa4c95c76b8d489889fc97702296beab
salman nafis <coachingwork2409@gmail.com>
2025-11-29 12:22:37 +0530

    Delete server/server directory

--- server/server/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ENTER YOUR PAYMENT GATEWAY SETTINGS HERE
-$merchant_id = "MERCHANT-ID-HERE";
-$secret_key  = "SECRET-KEY-HERE";
-
-// Your website PDF folder path
-$pdf_path_free  = "../pdf/class10-realnumbers.pdf";
-$pdf_path_paid  = "../pdf/class10-polynomials.pdf";
-?>
